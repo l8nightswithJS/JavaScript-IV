19465c346ac97c5544d4db9598d70f8ff113a6a3
eddie jimenez <ejmrtears89@gmail.com>
2019-08-16 16:43:31 -0700

    almost done with classes working on logs

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -85,6 +85,39 @@ Prototype Refactor
 */
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
+class GameObject {
+  constructor(attrs) {
+      this.createdAt = attrs.createdAt;
+      this.name = attrs.name;
+      this.dimensions = attrs.dimensions;
+  };
+  destroy = function() {
+      return (`${this.name} was removed from the game.`);
+      };
+}
+
+class CharacterStats extends GameObject {
+  constructor(attrs) {
+      super(attrs);
+      this.healthPoints = attrs.healthPoints;
+  };
+      takeDamage = function() {
+      return `${this.name} took damage.`
+      }
+}
+
+class Humanoid extends CharacterStats {
+  constructor(attrs) {
+      super(attrs);    
+      this.team = attrs.team;
+      this.weapons = attrs.weapons;
+      this.language = attrs.language;
+  };
+      greet = function() {
+      return `${this.name} offers a greeting in ${this.language}`;
+      };
+      
+}
 
 
   const mage = new Humanoid({
